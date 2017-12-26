46997b4eefe1cc499737a797bff886414619efc8
Richard Woods <rm-woods@hotmail.com>
2017-12-26 00:52:19 -0500

    Kicking "works" (untested)

--- Game2/Objects/Tetris.cs
+++ Game2/Objects/Tetris.cs
@@ -8,7 +8,8 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Game2.Objects
-{ 
+{
+
 	public enum Tetromino_Type
 	{
 		N = 0, // Null type
@@ -22,8 +23,48 @@ namespace Game2.Objects
 		Hl, // Highlight type
 		Hh, // Highlight type
 	}
+
+	public enum Kick_Offsets
+	{
+		N = -1,
+		ZR = 0,
+		RZ,
+		RT,
+		TR,
+		TL,
+		LT,
+		LZ,
+		ZL
+	}
+
 	class Tetris
 	{
+		/* JLSTZ Tetromino Kick Data */
+		public static readonly int[,,] JLSTZKickTable =
+		{
+/*0->R*/    { { 0, 0}, {-1, 0}, {-1, 1}, { 0,-2}, {-1,-2} },
+/*R->0*/    { { 0, 0}, {+1, 0}, {+1,-1}, { 0,+2}, {+1,+2} },
+/*R->2*/    { { 0, 0}, {+1, 0}, {+1,-1}, { 0,+2}, {+1,+2} },
+/*2->R*/	{ { 0, 0}, {-1, 0}, {-1,+1}, { 0,-2}, {-1,-2} },
+/*2->L*/    { { 0, 0}, {+1, 0}, {+1,+1}, { 0,-2}, {+1,-2} },
+/*L->2*/    { { 0, 0}, {-1, 0}, {-1,-1}, { 0,+2}, {-1,+2} },
+/*L->0*/    { { 0, 0}, {-1, 0}, {-1,-1}, { 0,+2}, {-1,+2} },
+/*0->L*/    { { 0, 0}, {+1, 0}, {+1,+1}, { 0,-2}, {+1,-2} }
+		};
+
+		/* I Tetromino Kick Data */
+		public static readonly int[,,] IKickTable =
+		{
+/*0->R*/    { { 0, 0}, {-2, 0}, {+1, 0}, {-2,-1}, {+1,+2} },
+/*R->0*/    { { 0, 0}, {+2, 0}, {-1, 0}, {+2,+1}, {-1,-2} },
+/*R->2*/    { { 0, 0}, {-1, 0}, {+2, 0}, {-1,+2}, {+2,-1} },
+/*2->R*/    { { 0, 0}, {+1, 0}, {-2, 0}, {+1,-2}, {-2,+1} },
+/*2->L*/    { { 0, 0}, {+2, 0}, {-1, 0}, {+2,+1}, {-1,-2} },
+/*L->2*/    { { 0, 0}, {-2, 0}, {+1, 0}, {-2,-1}, {+1,+2} },
+/*L->0*/    { { 0, 0}, {+1, 0}, {-2, 0}, {+1,-2}, {-2,+1} },
+/*0->L*/    { { 0, 0}, {-1, 0}, {+2, 0}, {-1,+2}, {+2,-1} }
+		};
+
 		private List<Tetromino> TetrominoQueue;
 		public Texture2D tetroTexture;
 		public Texture2D highlightTexture;
@@ -111,19 +152,28 @@ namespace Game2.Objects
 		}
 
 		// TODO: can move function, checks if able to move to location
-		private bool CanMove(Tetromino t, int fx, int fy)
+		private bool CanMove(Tetromino t, int fx, int fy, int r)
 		{
 			for (int x = 0; x < t.l_tetro.GetLength(2); x++)
 			{
 				for (int y = 0; y < t.l_tetro.GetLength(1); y++)
 				{
-					if (t.l_tetro[rotation, y, x] != 0 && fx + x >= 0 && fx + x < G.FieldWidth && fy + y >= 0 && fy + y < G.FieldHeight)
+					if (t.l_tetro[r, y, x] != 0)
 					{
-						if (l_field[fx + x, fy + y] != 0)
+						if (fx + x >= 0 && fx + x < G.FieldWidth && fy + y >= 0 && fy + y < G.FieldHeight)
+						{
+							if (l_field[fx + x, fy + y] != 0)
+							{
+								return false;
+							}
+						}
+						else
 						{
 							return false;
 						}
+						
 					}
+					
 				}
 			}
 			return true;
@@ -241,9 +291,9 @@ namespace Game2.Objects
 					Tetromino.DrawTetro(spriteBatch, tetroTexture, (Tetromino_Type)l_field[i, j], i, j);
 				}
 			}
-			TetrominoQueue.ElementAt(0).Draw(spriteBatch, tetroTexture, currentX, currentY, rotation);
-			TetrominoQueue.ElementAt(1).Draw(spriteBatch, tetroTexture, 30, 0, 0);
 			DrawGhost(spriteBatch);
+			TetrominoQueue.ElementAt(0).Draw(spriteBatch, tetroTexture, currentX, currentY, rotation);
+			TetrominoQueue.ElementAt(1).Draw(spriteBatch, tetroTexture, 15, 0, 0);
 		}
 		
 		public void Move(int direction)
@@ -270,21 +320,120 @@ namespace Game2.Objects
 
 		public void Rotate(int direction)
 		{
-			if (direction > 0 && rotation >= TetrominoQueue.ElementAt(0).l_tetro.GetLength(0) - 1)
+			if (TetrominoQueue.ElementAt(0).tetroType != Tetromino_Type.O)
 			{
-				rotation = 0;
-			}
-			else if (direction > 0)
-			{
-				rotation++;
-			}
-			else if (direction < 0 && rotation <= 0)
-			{
-				rotation = TetrominoQueue.ElementAt(0).l_tetro.GetLength(0) - 1;
-			}
-			else if (direction < 0)
-			{
-				rotation--;
+				int newr = -1;
+				if (direction > 0 && rotation >= TetrominoQueue.ElementAt(0).l_tetro.GetLength(0) - 1)
+				{
+					newr = 0;
+				}
+				else if (direction > 0)
+				{
+					newr = rotation + 1;
+				}
+				else if (direction < 0 && rotation <= 0)
+				{
+					newr = TetrominoQueue.ElementAt(0).l_tetro.GetLength(0) - 1;
+				}
+				else if (direction < 0)
+				{
+					newr = rotation - 1;
+				}
+
+				if (newr >= 0)
+				{
+					// TODO: this but better?
+					Kick_Offsets ko = Kick_Offsets.N;
+					switch(rotation)
+					{
+						case 0:
+							switch(newr)
+							{
+								case 1:
+									ko = Kick_Offsets.ZR;
+									break;
+								case 3:
+									ko = Kick_Offsets.ZL;
+									break;
+							}
+							break;
+						case 1:
+							switch (newr)
+							{
+								case 2:
+									ko = Kick_Offsets.RZ;
+									break;
+								case 0:
+									ko = Kick_Offsets.RT;
+									break;
+							}
+							break;
+						case 2:
+							switch (newr)
+							{
+								case 3:
+									ko = Kick_Offsets.TR;
+									break;
+								case 1:
+									ko = Kick_Offsets.TL;
+									break;
+							}
+							break;
+						case 3:
+							switch (newr)
+							{
+								case 0:
+									ko = Kick_Offsets.LT;
+									break;
+								case 2:
+									ko = Kick_Offsets.LZ;
+									break;
+							}
+							break;
+					}
+					if (ko >= 0)
+					{
+						Tetromino_Type tt = TetrominoQueue.ElementAt(0).tetroType;
+						int offsetindex = -1;
+						int x = -1;
+						int y = -1;
+						// TODO: hardcoded 5 kick checks
+						for (int i = 0; i < 5; i++)
+						{
+							switch (tt)
+							{
+								case Tetromino_Type.I:
+									x = currentX + IKickTable[(int)ko, i, 0];
+									y = currentY + IKickTable[(int)ko, i, 1];
+									break;
+								default:
+									x = currentX + JLSTZKickTable[(int)ko, i, 0];
+									y = currentY + JLSTZKickTable[(int)ko, i, 1];
+									break;
+							}
+							if (CanMove(TetrominoQueue.ElementAt(0), x, y, newr))
+							{
+								offsetindex = i;
+								break;
+							}
+						}
+						if (offsetindex >= 0)
+						{
+							switch (tt)
+							{
+								case Tetromino_Type.I:
+									currentX += IKickTable[(int)ko, offsetindex, 0];
+									currentY += IKickTable[(int)ko, offsetindex, 1];
+									break;
+								default:
+									currentX += JLSTZKickTable[(int)ko, offsetindex, 0];
+									currentY += JLSTZKickTable[(int)ko, offsetindex, 1];
+									break;
+							}
+							rotation = newr;
+						}
+					}
+				}
 			}
 		}
 		
